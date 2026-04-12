1557d7f1d4bc35a5ac7f880574351fdfbc128a57
BrianComposer <info@brianmartinez.music>
2026-04-12 14:21:48 +0200

    Experiment chart sty5

--- apps/run_train.py
+++ apps/run_train.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     ruta_base = sys.argv[1] if len(sys.argv) > 1 else None
-    model_name = sys.argv[2] if len(sys.argv) > 2 else "kurt001"
+    model_name = sys.argv[2] if len(sys.argv) > 2 else "mick001"
     corpus_name = sys.argv[3] if len(sys.argv) > 3 else "todos"
     token_name = sys.argv[4] if len(sys.argv) > 4 else "momet"
     save_epochs = sys.argv[5] if len(sys.argv) > 5 else False
