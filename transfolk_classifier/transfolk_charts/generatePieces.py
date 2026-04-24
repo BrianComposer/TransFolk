764bf999559966c1128a90ae3426568531d4d762
bsmartinez <brian.martinez@csmvalencia.es>
2026-04-24 10:53:33 +0200

    before curves

--- transfolk_classifier/transfolk_charts/generatePieces.py
+++
@@ -1,9 +0,0 @@
-
-import matplotlib.pyplot as plt
-
-plt.rcParams.update({
-    "text.usetex": False,
-    "font.family": "cmr10",  # usa Computer Modern integrada
-    "mathtext.fontset": "cm",  # activa Computer Modern para texto matemático y ejes
-})
-
